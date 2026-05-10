8a598d3e19814f10639f9ded5830093713efef17
erisan-noroff <192410141+erisan-noroff@users.noreply.github.com>
2026-05-10 21:45:49 +0200

    Refactored sign-in.js to separate submit event listener from button element function. Also spotted a bug. Immediate return was not added to error handling along the way.

--- public/assets/scripts/pages/sign-in.js
+++ public/assets/scripts/pages/sign-in.js
@@ -3,60 +3,74 @@ import { ToastMessage } from '../components/toast-messages.js';
 import formValidation from '../utils/form-validation.js';
 import { isAuthenticated, authApiClient } from '../utils/authentication.js';
 
-const homePage = new URL('index.html', window.location.href).toString();
-const formCard = document.querySelector('.form-card');
-const form = formCard?.querySelector('.form-card__form');
-
 function init() {
     if (isAuthenticated()) {
-        window.location.href = homePage;
+        window.location.href = new URL('index.html', window.location.href).toString();
         return;
     }
-    if (!formCard || !form) return;
 
-    signInButton();
+    const form = document.querySelector('.form-card__form');
+    if (!form) return;
+
+    const signInBtn = signInBtnElement();
+    form.insertBefore(signInBtn, form.lastElementChild);
+
+    submitEventListener();
     forgotPassword();
 }
 
-function signInButton() {
-    const signInButton = Button('Sign In', 'primary-btn primary-btn--full', 'sign-in', ButtonType.Submit);
-    form.insertBefore(signInButton, form.lastElementChild);
+function signInBtnElement() {
+    return Button('Sign In', 'primary-btn primary-btn--full', 'sign-in', ButtonType.Submit);
+}
 
-    form.addEventListener('submit', async(e) => {
+function submitEventListener() {
+    const form = document.querySelector('.form-card__form');
+    if (!form) return;
+
+    form.addEventListener('submit', async (e) => {
         const isValid = formValidation(e);
         if (!isValid) return;
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         const response = await authApiClient('login', { email, password });
-        if (!response) ToastMessage.apiDataLoadError();
-        
+        if (!response) { 
+            ToastMessage.apiDataLoadError();
+            return;
+        }
+
         const data = await response.json();
-        if (!response.ok) ToastMessage.error('Login failed', data.errors[0].message);
-        
+        if (!response.ok) {
+            ToastMessage.error('Login failed', data.errors[0].message);
+            return;
+        }
+
         const accessToken = data.data.accessToken;
-        if (!accessToken) ToastMessage.error('Unexpected server error', 'No authentication token received');
-        
+        if (!accessToken) {
+            ToastMessage.error('Unexpected server error', 'No authentication token received');
+            return;
+        }
+
         localStorage.setItem('auth_token', data.data.accessToken);
-        window.location.href = homePage;
+        window.location.href = new URL('index.html', window.location.href).toString();
     });
 }
 
 function forgotPassword() {
     const formCardRow = document.querySelector('.form-card__row');
-    if (formCardRow) {
-        const forgotPasswordButton = Button('Forgot password?', 'form-card__forgot-password');
-        formCardRow.append(forgotPasswordButton);
+    if (!formCardRow) return;
+    
+    const forgotPasswordButton = Button('Forgot password?', 'form-card__forgot-password');
+    formCardRow.append(forgotPasswordButton);
 
-        forgotPasswordButton.addEventListener('click', () => {
-            const email = document.getElementById('email');
-            if (!email.value.trim()) {
-                ToastMessage.error('Missing email address', 'Insert your email address to reset your password');
-                return;
-            }
+    forgotPasswordButton.addEventListener('click', () => {
+        const email = document.getElementById('email');
+        if (!email.value.trim()) {
+            ToastMessage.error('Missing email address', 'Insert your email address to reset your password');
+            return;
+        }
 
-            ToastMessage.success('Password Reset', 'If the account exists, a reset link has been sent to your email');
-        });
-    }
+        ToastMessage.success('Password Reset', 'If the account exists, a reset link has been sent to your email');
+    });
 }
 
 init();
